e5fc99f0ab14ecbd36a362436a1b0f5d74092d94
Vlad <vladobradley@mail.ru>
2022-08-30 00:23:09 +0600

    vue initialize

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+import { createApp } from "vue/dist/vue.esm-bundler";
+
+import TestComponent from "../views/components/TestComponent.vue";
+
+const app = createApp({
+    components: {
+        TestComponent
+    }
+});
+
+app.mount('#app')
---
+++ resources/views/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="test">
+    test-component
+</div>
+</template>
+
+<script>
+export default {
+    name: "TestComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
